2c945c7b409a6a9f7572520d50e735544f30e624
pradeenair <120032448+pradeenair@users.noreply.github.com>
2023-04-25 09:21:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import streamlit as st
 
-# Use st.cache_data to cache the processed dataframe
-@st.cache_data
-def process_data(file):
-    df = pd.read_excel(file, engine='openpyxl')
+# Use st.cache to cache the processed dataframe
+@st.cache
+def process_data(uploaded_file):
+    df = pd.read_excel(uploaded_file, engine='openpyxl')
     st.write("Original Dataframe:")
     st.write(df)
     try:
@@ -36,8 +36,8 @@ def main():
     st.set_page_config(page_title='User Journey Streamlit App', layout='wide')
     st.title('User Journey Streamlit App')
 
-    # Use st.cache_resource to cache the uploaded file
-    uploaded_file = st.cache_resource(st.file_uploader('Choose an Excel file', type=['xlsx', 'xls']))
+    # Use st.cache to cache the uploaded file
+    uploaded_file = st.cache(st.file_uploader)('Choose an Excel file', type=['xlsx', 'xls'])
 
     if uploaded_file is not None:
         df = process_data(uploaded_file)
